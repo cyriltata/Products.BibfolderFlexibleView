00e5800903920f6c3fd45bf6bf4f0aad144c679b
zopyx <yet@gmx.de>
2010-01-18 19:46:50 +0000

    restored full encoding functionality of the export form

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.3.6'
+version = '0.3.7'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
